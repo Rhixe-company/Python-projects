5d8c6a90a6d1951174ee1b10d5a36c1627b8ab38
rhixecompany <rhixecompany@gmail.com>
2025-03-22 19:51:38 +0100

    second commit

--- email sender.py
+++ email sender.py
@@ -1,27 +1,17 @@
 from email.message import EmailMessage
 import ssl
 import smtplib
-import environ
-from pathlib import Path
-BASE_DIR = Path(__file__).resolve(strict=True)
-env = environ.Env()
-READ_DOT_ENV_FILE = env.bool("READ_DOT_ENV_FILE", default=True)  # type: ignore  # noqa: PGH003
-if READ_DOT_ENV_FILE:
-    # OS environment variables take precedence over variables from .env
-    env.read_env(str(BASE_DIR / ".env"))
-email_sender = env(
-    "EMAIL_SENDER",
-    default="rhixecompany@gmail.com",  # type: ignore  # noqa: PGH003
-)
-email_password = env(
-    "EMAIL_PASSWORD",
-    default="",  # type: ignore  # noqa: PGH003
-)
-email_receiver = env(
-    "EMAIL_RECEIVER",
-    default="",  # type: ignore  # noqa: PGH003
-)
+import os
 
+email_sender = os.environ.get(
+    "EMAIL_SENDER"
+)
+email_password = os.environ.get(
+    "EMAIL_PASSWORD"
+)
+email_receiver = os.environ.get(
+    "EMAIL_RECEIVER"
+)
 subject = "Dont forget to subscribe"
 body = """
 When you watch a video, please hit subscribe
